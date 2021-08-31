62c623f76a1510989d625a8efee729bd15172ed8
Tim <tim.uniewski@gmail.com>
2021-08-31 17:02:49 +0200

    Example

--- TreeViewSample/ViewModels/HierarchyNodeViewModel.cs
+++ TreeViewSample/ViewModels/HierarchyNodeViewModel.cs
@@ -1,18 +1,26 @@
+using Avalonia.Dialogs;
+using ReactiveUI;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace TreeViewSample.ViewModels
 {
     public class HierarchyNodeViewModel : ViewModelBase
     {
+        public NavigationViewModel Parent { get; }
+
         public string Name { get; set; }
         public string Count { get => string.Format($"[{Children.Count}]"); }
         public ObservableCollection<HierarchyNodeViewModel> Children { get; } = new();
 
-        public HierarchyNodeViewModel(){}
+        public HierarchyNodeViewModel()
+        {
+        }
 
-        public HierarchyNodeViewModel(string name) : this()
+        public HierarchyNodeViewModel(string name, NavigationViewModel parent) : this()
         {
             Name = name;
+            Parent = parent;
         }
     }
 }
